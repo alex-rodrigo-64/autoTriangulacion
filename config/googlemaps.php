c0d811d0c14ba7a048285bbe561335a7f33dc177
alex-rodrigo-64 <alexcalle64@gmail.com>
2022-02-15 13:23:12 -0400

    da

--- config/googlemaps.php
+++ config/googlemaps.php
@@ -11,7 +11,7 @@ return [
     /* =====================================================================
     General
     ===================================================================== */
-    'key' => env('GOOGLE_MAPS_API_KEY', 'AIzaSyD3T_I3XRvnKbXL4ppS9boJpphoyh0igiw'), //Get API key: https://code.google.com/apis/console
+    'key' => env('GOOGLE_MAPS_API_KEY', 'AIzaSyCG0Mwpu_yN933__wTpo1-PusUGLlKokow'), //Get API key: https://code.google.com/apis/console
     'adsense_publisher_id' => env('GOOGLE_ADSENSE_PUBLISHER_ID', ''), //Google AdSense publisher ID
 
     'geocode' => [
